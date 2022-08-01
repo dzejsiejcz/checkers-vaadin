1d59d58e2f81631bfefc751e7edfa44a1d7badf7
Joel Chawraj <67972266+dzejsiejcz@users.noreply.github.com>
2022-08-01 21:55:16 +0200

    fix GitHub

--- src/main/java/com/checkers/web/quiz/QuizClient.java
+++ src/main/java/com/checkers/web/quiz/QuizClient.java
@@ -9,6 +9,13 @@ import java.util.*;
 @Component
 public class QuizClient {
 
+    public static final String URL_FOR_20_QUESTS = "https://the-trivia-api.com/api/questions?categories=food" +
+            "_and_drink,music,geography,science,general_knowledge,society" +
+            "_and_culture&limit=20&region=PL&difficulty=easy";
+    public static final String URL_FOR_1_QUEST = "https://the-trivia-api.com/api/questions?categories=" +
+            "food_and_drink,music,geography,science,general_knowledge," +
+            "society_and_culture&limit=1&region=PL&difficulty=easy";
+
     private final RestTemplate restTemplate;
 
     public QuizClient(RestTemplate restTemplate) {
@@ -17,9 +24,7 @@ public class QuizClient {
 
     public List<QuestionQuizDto> get20Questions() {
         QuestionQuizDto[] questionsResponse = restTemplate.getForObject(
-                "https://the-trivia-api.com/api/questions?categories=food" +
-                        "_and_drink,music,geography,science,general_knowledge,society" +
-                        "_and_culture&limit=20&region=PL&difficulty=easy",
+                URL_FOR_20_QUESTS,
                 QuestionQuizDto[].class
         );
         return Optional.ofNullable(questionsResponse)
@@ -29,9 +34,7 @@ public class QuizClient {
 
     public List<QuestionQuizDto> getOneQuestion() {
         QuestionQuizDto[] questionResponse = restTemplate.getForObject(
-                "https://the-trivia-api.com/api/questions?categories=" +
-                        "food_and_drink,music,geography,science,general_knowledge," +
-                        "society_and_culture&limit=1&region=PL&difficulty=easy",
+                URL_FOR_1_QUEST,
                 QuestionQuizDto[].class
         );
         return Optional.ofNullable(questionResponse)
